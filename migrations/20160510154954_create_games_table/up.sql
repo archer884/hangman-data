8ccc165a55aba8d45f989efa7db4e4b1e10d7670
J/A <archer884@gmail.com>
2016-05-11 17:00:43 -0500

    Add indexes for games

--- migrations/20160510154954_create_games_table/up.sql
+++ migrations/20160510154954_create_games_table/up.sql
@@ -3,4 +3,7 @@ create table games (
     token_id bigserial references tokens (id) not null,
     game_state text not null,
     outcome varchar(10) not null
-)
+);
+
+create index games_by_token_id on games (token_id);
+create index games_by_outcome on games (outcome);
