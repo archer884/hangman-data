ab48db1769d276c811a9075450153b3935dbc50e
J/A <archer884@gmail.com>
2016-05-11 16:24:22 -0500

    Add insert models

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,14 @@
+use schema::{games, tokens};
+
 #[derive(Queryable)]
 pub struct Token {
     pub id: i64,
     pub token: String,
+}
+
+#[insertable_into(tokens)]
+pub struct NewToken<'a> {
+    pub token: &'a str,
 }
 
 #[derive(Queryable)]
@@ -9,4 +16,12 @@ pub struct Game {
     pub id: i64,
     pub token_id: i64,
     pub game_state: String,
+    pub outcome: String,
+}
+
+#[insertable_into(games)]
+pub struct NewGame<'a> {
+    pub token_id: i64,
+    pub game_state: &'a str,
+    pub outcome: &'a str,
 }
